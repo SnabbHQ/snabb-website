036ce53e8645a58361748d52a6d1873259b8e647
Javier Tarazaga <javier.tarazaga@valtech.se>
2017-01-13 15:59:46 +0100

    First approach with PHP

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
-<?php header( 'Location: /es/index.html' ) ;  ?>
+<?php
+ $languages = array('en', 'es');
+ $header = explode(',', $_SERVER['HTTP_ACCEPT_LANGUAGE']);
+ foreach($header as $lang) {
+    if(in_array($lang, $languages)) {
+    header("Location: $lang.php"); // i.e. en.php or es.php
+        break;
+    }
+ }
+?>
